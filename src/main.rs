92beb2e21046eddab73982e9d49e550f7779900f
Mark Goodwin <markgoodwin@mdgmbp4.local>
2019-02-26 15:37:07 +0100

    Let's use rustfmt formatting rules

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-extern crate hyper;
 extern crate futures;
+extern crate hyper;
 
-use hyper::{Body, Response, Request, Server, Uri};
-use hyper::service::service_fn;
 use futures::future;
+use hyper::service::service_fn;
+use hyper::{Body, Request, Response, Server, Uri};
 
-use std::io::{self, Write};
-use hyper::Client;
 use hyper::rt::{self, Future, Stream};
+use hyper::Client;
+use std::io::{self, Write};
 
 static TEXT: &str = "Hello, World!";
 
 fn main() {
-    type BoxFut = Box<Future<Item=Response<Body>, Error=hyper::Error> + Send>;
+    type BoxFut = Box<Future<Item = Response<Body>, Error = hyper::Error> + Send>;
     let addr = ([0, 0, 0, 0], 3000).into();
 
     fn proxy(req: Request<Body>) -> BoxFut {
@@ -22,27 +22,29 @@ fn main() {
         match req.uri().host() {
             Some(hostname) => {
                 uri.authority(hostname);
-            },
+            }
             None => {
-                let host = req.headers().get("Host").expect("Sorry no host provided pal!");
+                let host = req
+                    .headers()
+                    .get("Host")
+                    .expect("Sorry no host provided pal!");
                 uri.authority(host.to_str().expect("header isn't a string"));
             }
         }
         uri.path_and_query(req.uri().path_and_query().expect("need a pathy").to_owned());
         let request_uri = uri.build().expect("doesn't look like a uri");
-println!("b {:?}", request_uri.to_string());
+        println!("b {:?}", request_uri.to_string());
         //rt::run(rt::lazy(|| {
-            let client = Client::new();
+        let client = Client::new();
 
-            Box::new(client
-                .get(request_uri))
-                /*
-                .map(|res| {
-                    println!("Response: {}", res.status());
-                })
-                .map_err(|err| {
-                    println!("Error: {}", err);
-                })*/
+        Box::new(client.get(request_uri))
+        /*
+        .map(|res| {
+            println!("Response: {}", res.status());
+        })
+        .map_err(|err| {
+            println!("Error: {}", err);
+        })*/
         //}));
         //Box::new(future::ok(Response::new(Body::from(request_uri.to_string()))))
     };
